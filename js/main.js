b515907134cfbb2f1257e870cffc6311e3e8ba50
xwaxes01 <xwaxes@gmail.com>
2020-11-11 12:33:44 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+function enlargeSearch(){
+    let search_bar = document.getElementById("search")
+    let search_view = document.getElementById("search-view")
+    search_bar.classList.add("focused")
+    search_view.classList.add("active")
+}
+
+function reduceSearch(){
+    let search_bar = document.getElementById("search")
+    let search_view = document.getElementById("search-view")
+    search_bar.classList.remove("focused")
+    search_view.classList.remove("active")
+}
+
+window.onclick = function (){
+    let search_bar = document.getElementById("search")
+    console.log(search_bar)
+    console.log(document.activeElement)
+    if(document.activeElement === search_bar){
+        enlargeSearch() 
+    } else{
+        reduceSearch()
+        console.log(search_bar.classList)
+    }
+}
